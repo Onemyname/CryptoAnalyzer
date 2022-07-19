a875a17f76a25590a17bdb51205437169bf8caec
Onemyname <konvvadim@yandex.ru>
2022-07-19 14:18:59 +0300

    add a working encoder and decoder using the Caesar method

---
+++ src/main/java/ru/javarush/cryptoanalyzer/konovalov/strategy/CaesarMethod/CaesarEncoder.java
@@ -0,0 +1,28 @@
+package ru.javarush.cryptoanalyzer.konovalov.strategy.CaesarMethod;
+
+import ru.javarush.cryptoanalyzer.konovalov.strategy.Actionable;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static ru.javarush.cryptoanalyzer.konovalov.controller.RegisterCommandService.getChosenCommand;
+import static ru.javarush.cryptoanalyzer.konovalov.strategy.CaesarMethod.CaesarMainCommandService.createCaesarCryptHashMap;
+import static ru.javarush.cryptoanalyzer.konovalov.strategy.CaesarMethod.CaesarMainCommandService.encodeOrDecodeFile;
+import static ru.javarush.cryptoanalyzer.konovalov.printer.Printable.println;
+
+public class CaesarEncoder implements Actionable {
+
+
+    public void action(String[] parameters) { // String[] args[] = {fileToEncrypt.txt, resultFile.txt, key}
+        println("Encryption attempt");
+        HashMap<Character, Character> cryptMap = createCaesarCryptHashMap(Integer.valueOf(parameters[2]), getChosenCommand());
+        try {
+            encodeOrDecodeFile(parameters[0], parameters[1], cryptMap);
+        } catch (IOException e) {
+            println("Error");
+        }
+
+
+    }
+}
+
---
+++ src/main/java/ru/javarush/cryptoanalyzer/konovalov/strategy/CaesarMethod/CaesarMainCommandService.java
@@ -0,0 +1,73 @@
+package ru.javarush.cryptoanalyzer.konovalov.strategy.CaesarMethod;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static ru.javarush.cryptoanalyzer.konovalov.reader.Reader.getReader;
+import static ru.javarush.cryptoanalyzer.konovalov.writer.Writer.getWriter;
+import static ru.javarush.cryptoanalyzer.konovalov.data.CryptAlphabetArray.getCryptAlphabetArrayLength;
+import static ru.javarush.cryptoanalyzer.konovalov.data.CryptAlphabetArray.getCryptArrayAlphabet;
+import static ru.javarush.cryptoanalyzer.konovalov.util.PathFinder.getRoot;
+
+public class CaesarMainCommandService {
+
+    static HashMap<Character, Character> createCaesarCryptHashMap(int key, int command) {
+        HashMap<Character, Character> map = new HashMap<>(getCryptAlphabetArrayLength());
+
+        char[] alphabet = getCryptArrayAlphabet();
+
+        char cryptValue;
+        if (command == 0) {
+            for (int i = 0; i < alphabet.length; i++) {
+                cryptValue = i + key <= alphabet.length - 1 ? alphabet[i + key] : alphabet[(key - (alphabet.length - i))];
+                map.put(alphabet[i], cryptValue);
+            }
+        } else if (command == 1) {
+            for (int i = 0; i < alphabet.length; i++) {
+                cryptValue = i - key < 0 ? alphabet[alphabet.length - (key - i)] : alphabet[i - key];
+                map.put(alphabet[i], cryptValue);
+            }
+        }
+        return map;
+    }
+
+    static void encodeOrDecodeFile(String fileToEncrypt, String resultFile, HashMap<Character, Character> cryptMap) throws IOException {
+        try (BufferedReader reader = getReader(getRoot() + fileToEncrypt);
+             BufferedWriter writer = getWriter(getRoot() + resultFile))
+        {
+            String currentLine = reader.readLine();
+            while (currentLine != null) {
+                String cryptoLine = encodeOrDecodeLine(currentLine, cryptMap);
+                writer.write(cryptoLine);
+                writer.append('\n');
+                currentLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String encodeOrDecodeLine(String line, HashMap<Character, Character> cryptMap) {
+
+        String[] linesArray = line.split(" ");
+
+        StringBuilder newWord = new StringBuilder();
+        ArrayList<StringBuilder> encodedLine = new ArrayList<>();
+        Character newSymbol;
+
+
+        for (String encodedWord : linesArray) {
+            for (int j = 0; j < encodedWord.length(); j++) {
+
+                char symbol = encodedWord.charAt(j);
+
+                newSymbol = cryptMap.getOrDefault(symbol, symbol);
+                newWord.append(newSymbol.toString());
+            }
+            encodedLine.add(newWord);
+            newWord = new StringBuilder();
+        }
+        return String.join(" ", encodedLine);
+    }
+}
